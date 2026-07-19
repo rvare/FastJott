5e5b0a71b498118560c1f284d193057028c3e9c2
Richard Varela <richardvare13@gmail.com>
2026-07-19 10:48:27 -0700

    Moved function new_note() to lib.rs for better modularity.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,17 @@
-use std::{env, fs, io, process};
-use std::io::Write;
-use chrono;
-
-fn new_note(important_flag: bool) {
-	let Some(mut file_path) = env::home_dir() else { // Returns a BufPath and moves it to file_path.
-		eprintln!("Could not get path to home directory!");
-		process::exit(1);
-	};
-
-	file_path.push("rtest.txt");
-
-	let mut file: fs::File = match fs::OpenOptions::new().append(true).create(true).open(file_path) {
-		Ok(file) => file,
-		Err(why) => {
-			eprintln!("Could not open notes.txt file: {}", why);
-			process::exit(1);
-		},
-	};
-
-	println!("Start typing your note below. Hit ENTER once you are done.");
-	let mut note_content = String::new();
-	if let Err(why) = io::stdin().read_line(&mut note_content) {
-		eprintln!("Input error: {}", why);
-		process::exit(1);
-	}
-
-	let current_date: chrono::DateTime<chrono::Local> = chrono::Local::now();
-
-	let signifier: &str = if important_flag { "(*) " } else { "" };
-
-	if let Err(why) = writeln!(file, "{}{}  {}", signifier, current_date.format("%Y-%m-%d"), note_content.trim()) {
-		eprintln!("Could not write to file: {}", why);
-		process::exit(1);
-	}
-
-	println!("\nNote saved.");
-}
+use std::env;
 
 fn main() {
 	let mut args_iter = env::args().skip(1);
 
 	if let Some(flag) = args_iter.next() {
 		match flag.as_str() {
-			"-i" => new_note(true),
+			"-i" => rustnoteutil::new_note(true),
 			_ => {
 				eprintln!("Unrecognized flag");
 				return;
 			}
 		}
 	} else {
-		new_note(false);
+		rustnoteutil::new_note(false);
 	}
 }
