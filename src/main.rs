df36769e7fdde1245aff43ef4cd66e97ca6f193d
Richard Varela <richardvare13@gmail.com>
2026-07-19 10:40:47 -0700

    Refactor new_note() to use eprintln and process::exit instead of panic! macro.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use std::{env, fs, io};
+use std::{env, fs, io, process};
 use std::io::Write;
 use chrono;
 
 fn new_note(important_flag: bool) {
 	let Some(mut file_path) = env::home_dir() else { // Returns a BufPath and moves it to file_path.
-		panic!("Could not get path to home directory!");
+		eprintln!("Could not get path to home directory!");
+		process::exit(1);
 	};
 
 	file_path.push("rtest.txt");
 
 	let mut file: fs::File = match fs::OpenOptions::new().append(true).create(true).open(file_path) {
 		Ok(file) => file,
-		Err(why) => panic!("Could not open notes.txt file: {}", why),
+		Err(why) => {
+			eprintln!("Could not open notes.txt file: {}", why);
+			process::exit(1);
+		},
 	};
 
 	println!("Start typing your note below. Hit ENTER once you are done.");
 	let mut note_content = String::new();
 	if let Err(why) = io::stdin().read_line(&mut note_content) {
-		panic!("Input error: {}", why);
+		eprintln!("Input error: {}", why);
+		process::exit(1);
 	}
 
 	let current_date: chrono::DateTime<chrono::Local> = chrono::Local::now();
@@ -25,7 +30,8 @@ fn new_note(important_flag: bool) {
 	let signifier: &str = if important_flag { "(*) " } else { "" };
 
 	if let Err(why) = writeln!(file, "{}{}  {}", signifier, current_date.format("%Y-%m-%d"), note_content.trim()) {
-		panic!("Could not write to file: {}", why);
+		eprintln!("Could not write to file: {}", why);
+		process::exit(1);
 	}
 
 	println!("\nNote saved.");
