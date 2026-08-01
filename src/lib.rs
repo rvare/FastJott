9d3e1f7b2a948a24927df35e8aa18c6d5168d184
Richard Varela <richardvare13@gmail.com>
2026-08-01 15:39:56 -0700

    Ran cargo fmt to reformate all source files to follow Rust conventions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,70 @@
-use std::{env, fs, io, process};
-use std::io::Write;
 use chrono;
+use std::io::Write;
+use std::{env, fs, io, process};
 
 pub fn new_note(important_flag: bool) {
-	let Some(mut file_path) = env::home_dir() else { // Returns a PathBuf and moves it to file_path.
-		eprintln!("Could not get path to home directory!");
-		process::exit(1);
-	};
+    let Some(mut file_path) = env::home_dir() else { // Returns a PathBuf and moves it to file_path.
+        eprintln!("Could not get path to home directory!");
+        process::exit(1);
+    };
 
-	file_path.push("rtest.txt");
+    file_path.push("rtest.txt");
 
-	let mut file: fs::File = match fs::OpenOptions::new().append(true).create(true).open(file_path) {
-		Ok(file) => file,
-		Err(why) => {
-			eprintln!("Could not open notes.txt file: {}", why);
-			process::exit(1);
-		},
-	};
+    let mut file: fs::File = match fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_path)
+    {
+        Ok(file) => file,
+        Err(why) => {
+            eprintln!("Could not open notes.txt file: {}", why);
+            process::exit(1);
+        }
+    };
 
-	println!("Start typing your note below. Hit ENTER once you are done.");
-	let mut note_content = String::new();
-	if let Err(why) = io::stdin().read_line(&mut note_content) {
-		eprintln!("Input error: {}", why);
-		process::exit(1);
-	}
+    println!("Start typing your note below. Hit ENTER once you are done.");
+    let mut note_content = String::new();
+    if let Err(why) = io::stdin().read_line(&mut note_content) {
+        eprintln!("Input error: {}", why);
+        process::exit(1);
+    }
 
-	let current_date: chrono::DateTime<chrono::Local> = chrono::Local::now();
+    let current_date: chrono::DateTime<chrono::Local> = chrono::Local::now();
 
-	let signifier: &str = if important_flag { "(*) " } else { "" };
+    let signifier: &str = if important_flag { "(*) " } else { "" };
 
-	if let Err(why) = writeln!(file, "{}{}  {}", signifier, current_date.format("%Y-%m-%d"), note_content.trim()) {
-		eprintln!("Could not write to file: {}", why);
-		process::exit(1);
-	}
+    if let Err(why) = writeln!(
+        file,
+        "{}{}  {}",
+        signifier,
+        current_date.format("%Y-%m-%d"),
+        note_content.trim()
+    ) {
+        eprintln!("Could not write to file: {}", why);
+        process::exit(1);
+    }
 
-	println!("\nNote saved.");
+    println!("\nNote saved.");
 }
 
 pub fn archive_notes_file() {
-	let Some(mut notes_path) = env::home_dir() else { // Returns a PathBuf and moves it to file_path.
-		eprintln!("Could not get path to home directory!");
-		process::exit(1);
-	};
+    let Some(mut notes_path) = env::home_dir() else { // Returns a PathBuf and moves it to file_path.
+        eprintln!("Could not get path to home directory!");
+        process::exit(1);
+    };
 
-	let mut archive_path = notes_path.clone();
+    let mut archive_path = notes_path.clone();
 
-	notes_path.push("rtest.txt");
+    notes_path.push("rtest.txt");
 
-	archive_path.push("NotestxtBackUp");
-	let current_date: chrono::DateTime<chrono::Local> = chrono::Local::now();
-	archive_path.push(format!("archive_{}.txt", current_date.format("%Y%m%d")));
+    archive_path.push("NotestxtBackUp");
+    let current_date: chrono::DateTime<chrono::Local> = chrono::Local::now();
+    archive_path.push(format!("archive_{}.txt", current_date.format("%Y%m%d")));
 
-	if let Err(why) = fs::copy(&notes_path, &archive_path) {
-		eprintln!("Input error: {}", why);
-		process::exit(1);
-	};
+    if let Err(why) = fs::copy(&notes_path, &archive_path) {
+        eprintln!("Input error: {}", why);
+        process::exit(1);
+    };
 
-	println!("Archived to: {}", archive_path.display());
+    println!("Archived to: {}", archive_path.display());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::env;
 
 fn main() {
-	let mut args_iter = env::args().skip(1);
+    let mut args_iter = env::args().skip(1);
 
-	if let Some(flag) = args_iter.next() {
-		match flag.as_str() {
-			"-i" => rustnoteutil::new_note(true),
-			"-a" => rustnoteutil::archive_notes_file(),
-			_ => {
-				eprintln!("Unrecognized flag");
-				return;
-			}
-		}
-	} else {
-		rustnoteutil::new_note(false);
-	}
+    if let Some(flag) = args_iter.next() {
+        match flag.as_str() {
+            "-i" => rustnoteutil::new_note(true),
+            "-a" => rustnoteutil::archive_notes_file(),
+            _ => {
+                eprintln!("Unrecognized flag");
+                return;
+            }
+        }
+    } else {
+        rustnoteutil::new_note(false);
+    }
 }
